afd8d8ec32da49d50b58123badebe45dd5106d7f
KT252005 <23f2005464@ds.study.iitm.ac.in>
2024-08-15 23:24:47 +0530

    update settings

--- chat_personal/settings.py
+++ chat_personal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'chat_personal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["chat_personal\\templates"],
+        'DIRS': [os.path.join(BASE_DIR,"chat_personal/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
